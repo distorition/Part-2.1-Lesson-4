bc7c8d6c4696174ec1d36da71c34b13e182c8451
distorition <85959781+distorition@users.noreply.github.com>
2021-10-28 17:30:33 +0300

    Add files via upload

---
+++ Part 2 lesson 4.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Part_2_lesson_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Houses houses = new Houses(1, 2, 3, 5);
+            houses.Show();
+            houses.TakesDoors();
+            houses.CreatBulild(1, 2, 3, 4);
+            Console.WriteLine();
+           
+            
+           
+        }
+    }
+}
